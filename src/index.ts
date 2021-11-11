45bc59115eb7efda253e72a13b1bb5b9229875ec
AidaHerreravilar <92064121+AidaHerreravilar@users.noreply.github.com>
2021-11-11 20:53:49 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+let menorValor : number =3
+let mayorValor : number =7
+let edadesAlumnos : [] = new Array(10)
+let sumaEdades : number;
+function aleatorio(mayorValor: number, menorValor) {
+  return Mat.floor(mat.random () * (mayorValor - menorValor +1)) + menorValor;
+ 
+  for (let i 0; = 0; < edades.Alumnos.length; i++) {
+    edadesAlumnos = arr0;];
+    
+  }
+  
+}
